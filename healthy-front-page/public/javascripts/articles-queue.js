3aae1a20b8175f2cd62ad8e433b8cee62d038848
Miroslaw Kucharzyk <miroslaw.kucharzyk@schibsted.pl>
2013-12-12 14:57:32 +0100

    remove button in class

--- healthy-front-page/public/javascripts/articles-queue.js
+++ healthy-front-page/public/javascripts/articles-queue.js
@@ -5,13 +5,22 @@ function ready() {
     var articleDataMap = 'data-id';
     var articlesQueue = new ArticlesQueue('.drop-articles-here', 'published', articleDataMap);
 
-        $('.drop-articles-here').on("click",".article-add", function() {
-            var parentId = $(this).closest('li').attr(articleDataMap);
-            articlesQueue.add(parentId);
-        });
+    var addCallback = function() {
+        socket.emit('added', {});
+    }
+
+    $('.drop-articles-here').on("click",".article-add", function() {
+        var parentId = $(this).closest('li').attr(articleDataMap);
+        articlesQueue.add(parentId);
+    });
+
+    $('.gridster').on("click",".article-remove", function() {
+        var parentId = $(this).closest('li').attr(articleDataMap);
+        articlesQueue.add(parentId);
+    });
 }
 
-function ArticlesQueue(id, publishedClassName, articleDataMap) {
+function ArticlesQueue(id, publishedClassName, articleDataMap, addCb) {
     var gridster = $(".gridster ul").gridster().data('gridster');
     return {
         add: function(articleId) {
@@ -22,8 +31,9 @@ function ArticlesQueue(id, publishedClassName, articleDataMap) {
                     return true;
                 }
             });
-            listElement.addClass(publishedClassName);
+            listElement.find('.article-add').removeClass('article-add').addClass('article-remove');
             gridster.add_widget(listElement);
+            addCb();
         },
         remove: {
 
