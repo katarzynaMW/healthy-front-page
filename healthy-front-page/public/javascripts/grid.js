3778d9bf358308f33b1b1ad0e1bbd5f26f1d12f1
Cezar “ikari” Pokorski <git@ikari.pl>
2013-12-12 15:11:19 +0100

    correct element lookup on filling articles

--- healthy-front-page/public/javascripts/grid.js
+++ healthy-front-page/public/javascripts/grid.js
@@ -39,7 +39,7 @@ hfp.buildFromSerialized = function(serialized) {
         var $newTile, id = this.id;
         hfp.gridster.add_widget('<li data-id="' + id + '" />', this.size_x, this.size_y, this.col, this.row);
         $newTile = $('.gridster li[data-id="' + id + '"]').eq(0);
-        if (id && $newTile.children().length == 0) {
+        if (id && $newTile.find('article').length == 0) {
             // please populate me
             console.log('tile ' + id + ' needs populating');
             $.get('/article/' + id, function(data) {
