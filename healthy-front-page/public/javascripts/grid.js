9036ed578c13c540fee2ebd6976e122f692d139f
Cezar “ikari” Pokorski <git@ikari.pl>
2013-12-12 10:10:32 +0100

    gridster scripts

---
+++ healthy-front-page/public/javascripts/grid.js
@@ -0,0 +1,9 @@
+$(".gridster ul").gridster({
+    widget_margins: [5, 5],
+    widget_base_dimensions: [100, 100],
+    autogenerate_stylesheet: true,
+    resize: {
+        enabled: true,
+        max_size: [3, 3]
+    }
+});
